2aad242d86653e42154b13aed12e435163dd91e2
iceonGit <icey58506@gmail.com>
2023-03-30 19:45:56 +0300

    fixing some errors

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <p className="copyright">&copy; Deadman's Factory</p>
+      <p className="copyright">&copy; Deadman&apos;s Factory</p>
     </div>
   );
 };
